2ec11c87d46ea8f6bbd8b9324a2cdd882c533504
keita <keita_broadwater@yahoo.com>
2016-08-06 08:26:23 -0700

    adjusting bootstrap files

--- gait/gait/settings.py
+++ gait/gait/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main_app'
+    'main_app',
+    'bootstrap3'
 ]
 
 MIDDLEWARE_CLASSES = [
